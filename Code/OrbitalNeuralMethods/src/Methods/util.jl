f6290629afeadf5a8c422db63cada1f973dabfcc
KarlHenrik <40461324+KarlHenrik@users.noreply.github.com>
2022-08-19 16:09:20 +0200

    more results

--- Code/OrbitalNeuralMethods/src/Methods/util.jl
+++ Code/OrbitalNeuralMethods/src/Methods/util.jl
@@ -1,17 +1,19 @@
 function compute_ground_state!(state; max_iter = 1000, tol = 1e-10, verbose = false)
     old_E = energy(state)
-    
+    delta_E = 0
     for i in 1:max_iter
         update!(state)
         
         new_E = energy(state)
+        delta_E = old_E - new_E
         if verbose
             println("Iteration $i: E = $new_E")
         end
-        if abs(new_E - old_E) < tol
+        if abs(delta_E) < tol
             return state
         end
         old_E = new_E
     end
+    println("Did not converge after $(max_iter) iterations. Final energy change was $(delta_E)")
     return state
 end
